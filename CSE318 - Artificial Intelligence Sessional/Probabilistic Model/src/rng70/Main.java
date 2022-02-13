e01493f56616caae1e01cd84cbef5505cb334410
rng70 <1705070@ugrad.cse.buet.ac.bd>
2022-02-13 08:26:51 +0600

    offline completed

--- CSE318 - Artificial Intelligence Sessional/Probabilistic Model/src/rng70/Main.java
+++ CSE318 - Artificial Intelligence Sessional/Probabilistic Model/src/rng70/Main.java
@@ -1,12 +1,12 @@
 package rng70;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
-        System.out.println("Nothing is good here!!!");
+        DecimalFormat df_obj = new DecimalFormat("#.###");
         System.out.println("Please enter the input");
 
         int n, m, k;
@@ -25,24 +25,38 @@ public class Main {
             board[r][c] = -1.0;
         }
 
-        /**
-         * @brief initial probability of each cell of the board
+        /*
+         * initial probability of each cell of the board
          * will be same as 1/(n*m-k) where k is the cell with
          * obstacle where Casper cannot stay
          */
         model.SetInitialBoard(n*m-k);
+        model.InitialBoardStatus();
 
         String s;
+        int counter = 0;
         while(true){
             s = scanner.nextLine();
-            if(s.charAt(0)=='Q' || s.charAt(0)=='q'){
-                break;
-            }
-            if(s.charAt(0)=='C' || s.charAt(0)=='c'){
-                model.queryCasperPosition();
-            }else if(s.charAt(0)=='R' || s.charAt(0)=='r'){
-                String[] arrOfStr = s.split(" ");
-                model.ProcessModel(Integer.parseInt(arrOfStr[1]), Integer.parseInt(arrOfStr[2]), Integer.parseInt(arrOfStr[3]));
+            if(s.length()>0){
+                if(s.charAt(0)=='Q' || s.charAt(0)=='q'){
+                    break;
+                }
+                if(s.charAt(0)=='C' || s.charAt(0)=='c'){
+                    model.QueryCasperPosition();
+                }else if(s.charAt(0)=='R' || s.charAt(0)=='r'){
+                    counter++;
+                    String[] arrOfStr = s.split(" ");
+
+                    System.out.println("Probability Update (" + counter + "-th Reading):");
+                    String str;
+                    if(Integer.parseInt(arrOfStr[3])==1){
+                        str = "[The probabilities of the cells around ("+Integer.parseInt(arrOfStr[1])+ ", " + Integer.parseInt(arrOfStr[2]) + ") will increase while those of others may decrease.]";
+                    }else{
+                        str = "[The probabilities of the cells around ("+Integer.parseInt(arrOfStr[1])+ ", " + Integer.parseInt(arrOfStr[2]) + ") will decrease while those of others may increase.]";
+                    }
+                    System.out.println(str);
+                    model.ProcessModel(Integer.parseInt(arrOfStr[1]), Integer.parseInt(arrOfStr[2]), Integer.parseInt(arrOfStr[3]));
+                }
             }
         }
     }
--- CSE318 - Artificial Intelligence Sessional/Probabilistic Model/src/rng70/Model.java
+++ CSE318 - Artificial Intelligence Sessional/Probabilistic Model/src/rng70/Model.java
@@ -1,31 +1,42 @@
 package rng70;
 
+import java.text.DecimalFormat;
+
 public class Model {
-    double p;
+    double p, g;
     double[][] board;
     double[][] evidence;
+    double[][] nonNormalizedBoard;
     int n, m, k;
+
+    DecimalFormat df_obj = new DecimalFormat("#.####");
     public Model(int n, int m, int k){
         this.n = n;
         this.m = m;
         this.k = k;
         this.p = 0.9;
+        this.g = 0.85;
 
         board = new double[n][m];
         evidence = new double[n][m];
+        nonNormalizedBoard = new double[n][m];
 
         SetInitialBoard(board);
         SetInitialBoard(evidence);
+        SetInitialBoard(nonNormalizedBoard);
     }
 
-    public boolean isCellBlocked(int r, int c){
-        return board[r][c] != -1.0;
+    private boolean isCellBlocked(int r, int c){
+        return board[r][c] == -1.0;
     }
 
-    public double getCornerProbability(int r, int c){
-        int count = 0;
-//        if((r-1)>-1 && !isCellBlocked(r-1, c))
-//            count++;
+    private boolean isValidCell(int r, int c){
+        return (r >= 0 && r < n) && (c >= 0 && c < m);
+    }
+
+    private double getCornerProbability(int r, int c){
+        int count = 1;
+
         if(((r-1)>-1 && (c-1)>-1) && !isCellBlocked(r-1, c-1))
             count++;
         if(((r-1)>-1 && (c+1)<m) && !isCellBlocked(r-1, c+1))
@@ -34,30 +45,18 @@ public class Model {
             count++;
         if(((r+1)<n && (c-1)>-1) && !isCellBlocked(r+1, c-1))
             count++;
-//        if((r+1)<n && !isCellBlocked(r+1, c))
-//            count++;
-//        if((c-1)>-1 && ! isCellBlocked(r, c-1))
-//            count++;
-//        if((c+1)<m && !isCellBlocked(r, c+1))
-            count++;
 
-        // TODO need to check if the count is 0
-        final double v = (1-p) / (double) count;
-        return v;
+        return (1-p) / (double) count;
+    }
+
+    public void InitialBoardStatus(){
+        System.out.println("\n\n[A " + n + "X" + m + " grid where each cell without obstacle has a probability value = 1/" + (n*m-k) + ", Each cell with obstacle has a probability value = 0.]");
     }
 
     public double getEdgeProbability(int r, int c){
         int count = 0;
         if((r-1)>-1 && !isCellBlocked(r-1, c))
             count++;
-//        if(((r-1)>-1 && (c-1)>-1) && !isCellBlocked(r-1, c-1))
-//            count++;
-//        if(((r-1)>-1 && (c+1)<m) && !isCellBlocked(r-1, c+1))
-//            count++;
-//        if(((r+1)<n && (c+1)<m) && !isCellBlocked(r+1, c+1))
-//            count++;
-//        if(((r+1)<n && (c-1)>-1) && !isCellBlocked(r+1, c-1))
-//            count++;
         if((r+1)<n && !isCellBlocked(r+1, c))
             count++;
         if((c-1)>-1 && ! isCellBlocked(r, c-1))
@@ -66,8 +65,9 @@ public class Model {
             count++;
         
         // TODO need to check if the count is 0
-        final double v = p / (double) count;
-        return v;
+        if(count == 0)
+            return 0;
+        return p / (double) count;
     }
 
     public double[][] getBoard(){
@@ -82,43 +82,163 @@ public class Model {
         }
     }
     public void PrintBoard(){
+        System.out.println("<++++++++++++++++++++++++++++++++++>");
         System.out.println("Hidden Markov Model");
         double sum = 0.0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                System.out.print(board[i][j] + " ");
+                System.out.print(df_obj.format(board[i][j]) + " ");
                 sum += board[i][j];
             }
             System.out.println();
         }
-        System.out.println("Total Probability is : "+ sum);
+        System.out.println("Total Probability is : "+ df_obj.format(sum));
+        System.out.println("<++++++++++++++++++++++++++++++++++>\n");
+
     }
 
     public void SetInitialBoard(int value){
         double pValue = 1.0 / (value * 1.0);
         for (int i=0 ;i<n;i++){
-            for (int j=0;j<m;j++)
-                if(board[i][j] != -1.0 )
+            for (int j=0;j<m;j++) {
+                if (board[i][j] != -1.0) {
                     board[i][j] = pValue;
+                }
+            }
         }
-
     }
 
     public void PrintEvidence(){
+        System.out.println("<++++++++++++++++++++++++++++++++++>");
+        System.out.println("Evidence matrix");
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 System.out.print(evidence[i][j]+" ");
             }
             System.out.println();
         }
+        System.out.println("<++++++++++++++++++++++++++++++++++>\n");
+    }
+
+    public double GetCornerCumulativeProb(int r, int c){
+        double cProb = 0.0;
+        if(isValidCell(r-1, c-1)){
+            if(!isCellBlocked(r-1, c-1)){
+                cProb += board[r-1][c-1] * getCornerProbability(r, c);
+            }
+        }
+        if(isValidCell(r-1, c+1)){
+            if(!isCellBlocked(r-1, c+1)){
+                cProb += board[r-1][c+1] * getCornerProbability(r, c);
+            }
+        }
+        if(isValidCell(r+1, c-1)){
+            if(!isCellBlocked(r+1, c-1)){
+                cProb += board[r+1][c-1] * getCornerProbability(r, c);
+            }
+        }
+        if(isValidCell(r+1, c+1)){
+            if(!isCellBlocked(r+1, c+1)){
+                cProb += board[r+1][c+1] * getCornerProbability(r, c);
+            }
+        }
+        // special case: own cell
+        if(isValidCell(r, c)){
+            if(!isCellBlocked(r, c)){
+                cProb += board[r][c] * getCornerProbability(r, c);
+            }
+        }
+
+        return cProb;
+    }
+
+    public double GetAdjCumulativeProb(int r, int c){
+        double cProb = 0.0;
+        if(isValidCell(r-1, c)){
+            if(!isCellBlocked(r-1, c)){
+                cProb += board[r-1][c] * getEdgeProbability(r, c);
+            }
+        }
+        if(isValidCell(r+1, c)){
+            if(!isCellBlocked(r+1, c)){
+                cProb += board[r+1][c] * getEdgeProbability(r, c);
+            }
+        }
+        if(isValidCell(r, c-1)){
+            if(!isCellBlocked(r, c-1)){
+                cProb += board[r][c-1] * getEdgeProbability(r, c);
+            }
+        }
+        if(isValidCell(r, c+1)){
+            if(!isCellBlocked(r, c+1)){
+                cProb += board[r][c+1] * getEdgeProbability(r, c);
+            }
+        }
+        return cProb;
+    }
+
+    public void UpdateEvidenceMatrix(int r, int c, int b){
+        double prob;
+        if(b==1){
+            prob = g;
+        }else {
+            prob = 1 - g;
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+               if(i==r || i==(r-1) || i==(r+1)){
+                   if(j==c || j==(c-1) || j==(c+1)){
+                        evidence[i][j] = prob;
+                   }else{
+                       evidence[i][j] = 1-prob;
+                   }
+               }else{
+                   evidence[i][j] = 1-prob;
+               }
+            }
+        }
+    }
+    public void Normalize(double[][] tempBoard, double sum){
+        for(int i=0;i<tempBoard.length;i++){
+            for(int j=0;j<tempBoard[i].length;j++){
+                board[i][j] = tempBoard[i][j]/sum;
+            }
+        }
     }
 
     public void ProcessModel(int a, int b, int c){
-
+        double sum = 0.0;
+        UpdateEvidenceMatrix(a, b, c);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(!isCellBlocked(i, j)){
+                    double value = GetAdjCumulativeProb(i, j) + GetCornerCumulativeProb(i, j);
+                    nonNormalizedBoard[i][j]=value*evidence[i][j];
+                    sum += nonNormalizedBoard[i][j];
+                }
+            }
+        }
+        Normalize(nonNormalizedBoard, sum);
+        PrintBoard();
     }
 
-    public void queryCasperPosition(){
+    public void QueryCasperPosition(){
         // TODO
-        System.out.println("TODO");
+        PrintBoard();
+        int maxR = -1, maxC = -1;
+        double maxP = board[0][0];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+
+                if(board[i][j] > maxP){
+//                    System.out.print(df_obj.format(board[i][j]) + " > " + df_obj.format(maxP) + " = true");
+                    maxR = i;
+                    maxC = j;
+                    maxP = board[i][j];
+                }
+            }
+        }
+        System.out.println("[Casper is most probably at (" + (maxR+1) + "," + (maxC) + ") [assuming it has the highest probability value among the cells]]");
     }
 }
